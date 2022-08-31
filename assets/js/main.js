f495dd138ba4a81846c63e154bd647da2af1dafb
unknown <ajibolamubarak304@gmail.com>
2022-08-31 13:07:33 +0100

    JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,16 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 /*=============== SWIPER POPULAR ===============*/
 
-
 /*=============== VALUE ACCORDION ===============*/
-
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+/*=============== SHOW SCROLL UP ===============*/
 
-/*=============== SHOW SCROLL UP ===============*/ 
-
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
