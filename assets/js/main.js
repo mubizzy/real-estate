0b0fe4e34264c79224fdf87e18a023fa56b15bdf
unknown <ajibolamubarak304@gmail.com>
2022-08-31 13:21:45 +0100

    JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,11 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-const navMenu = document.getElementById("nav-menu"),
-  navToggle = document.getElementById("nav-toggle"),
-  navClose = document.getElementById("nav-close");
-
+function scrollHeader() {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 80) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
 /*=============== SWIPER POPULAR ===============*/
 
 /*=============== VALUE ACCORDION ===============*/
