d4e9e47d1b205e3d5b36a14cc9588e0fe7ea5b97
unknown <ajibolamubarak304@gmail.com>
2022-09-05 14:22:07 +0100

    JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -93,4 +93,14 @@ const getCurrentTheme = () =>
   document.body.classList.contains(darkTheme) ? "dark" : "light";
 const getCurrentIcon = () =>
   themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+    iconTheme
+  );
+}
 /*=============== SCROLL REVEAL ANIMATION ===============*/
